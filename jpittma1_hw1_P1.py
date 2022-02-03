a0e6413b11f4582091ce1ef6a4b6f7a9b19c7753
jpittma1 <91224170+jpittma1@users.noreply.github.com>
2022-02-03 07:38:30 -0500

    Update jpittma1_hw1_P1.py

--- jpittma1_hw1_P1.py
+++ jpittma1_hw1_P1.py
@@ -10,20 +10,21 @@ import matplotlib.pyplot as plt
 import math
 from sympy import N
 
-#----PROBLEM 1----
+##----PROBLEM 1----
 #--Part 1: Compute Field of View
-width=0.014 #14mm square shaped
-height=0.014
+camera_width=0.014 #14mm square shaped
+camera_height=0.014
 focal_length=0.025 #25mm
 
 #angle/2=arctan(retina/2focalLength)
-width_halfAngle=math.atan2(width,(2*focal_length))
-height_halfAngle=math.atan2(height,(2*focal_length))
+width_halfAngle=math.atan2(camera_width,(2*focal_length))
+height_halfAngle=math.atan2(camera_height,(2*focal_length))
 VFOV=2*width_halfAngle
 HFOV=2*height_halfAngle
 
 print("Horizontal Field of View is: ", N(HFOV,4))
 print("Vertical Field of View is: ",N(VFOV,4))
+
 
 #--Part 2: Compite minimum number of pixels object occupy in image
 object_width=0.50 #5cm
@@ -34,13 +35,7 @@ pixel_width=math.sqrt(resolution)
 pixel_height=pixel_width
 #print(N(pixel_width,7))
 
-x_prime=(pixel_width*focal_length)/z
-print(x_prime)
-y_prime=(pixel_height*focal_length)/z
+object_pixel_height=(focal_length*object_height*pixel_height)/(z*camera_height)
+min_pixels_object=object_pixel_height*object_pixel_height
 
-projected_pixels=x_prime*y_prime
-print(projected_pixels)
-
-print("\nBased on a 5 MPix camera with object 20 meters away, ")
-print("The object is projected at ", N(x_prime,5),  "horizontal pixels and ")
-print(N(y_prime,5), "pixels vertically, equalling ", N(projected_pixels,5), "pixels in total")
+print("The minimum number of pixels occupied by the object is ", min_pixels_object, " or ", round(min_pixels_object), "rounded to whole pixel")
