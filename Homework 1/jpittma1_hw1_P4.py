2289d56dd556e9c7edefac59cceb42cee8d47ff6
jpittma1 <91224170+jpittma1@users.noreply.github.com>
2022-02-07 11:56:52 -0500

    Update jpittma1_hw1_P4.py

--- Homework 1/jpittma1_hw1_P4.py
+++ Homework 1/jpittma1_hw1_P4.py
@@ -24,6 +24,7 @@ def computeSVD(A):
     #sort values and vectors
     sort_U=eig_val_U.argsort()[::-1]
     sorted_val_U=eig_val_U[sort_U]
+    # print("sorted val U shape", sorted_val_U.shape) #8
     U=eig_vect_U[:,sort_U]
         
     #STEP 2: Solve for V transpose matrix (VT)
@@ -35,21 +36,33 @@ def computeSVD(A):
     V=eig_vect_V[:,sort_V]
     VT=V.transpose()
     
+    
+    # sorted_val_U=eigenvalue[sort]
+    
+    #remove negative eigenvalues
+    for i in range(len(sorted_val_U)):
+        if sorted_val_U[i]<=0:
+            sorted_val_U[i]*=-1
+            
     #STEP 3: Solve for Sigma Matrix (E)
     #square-rooted diagonal matrix of U eigenvalues squared
-    # diag=np.diag((np.sqrt(sorted_val_U)))
-    # print("diag shape", diag.shape)
-    # sigma=np.zeros_like(A).astype(np.float64)
-    # print("sigma shape", sigma.shape)
-    # sigma[:diag.shape[0],:diag.shape[1]]=diag #!NOT Square so can't go from 8x9 to 9x9
+    diag=np.diag((np.sqrt(sorted_val_U)))
+    # print("diag shape", diag.shape) #9x9
+
+    sigma=np.zeros_like(A).astype(np.float64) #8x9
+   
+    for i in range(len(sigma)):
+        sigma[i][i]=diag[i][i]
+        
+    # print("sigma shape", sigma.shape) #8x9
     
     #STEP 4: solve for Homography Matrix (H)
     #U.E.VT
     H=V[:,8]  #H is last column of V
     H=np.reshape(H,(3,3)) #assigment wants H in a 3x3 matrix format
     
-    # return VT, U, sigma, H
-    return VT, U, H
+    return VT, U, sigma, H
+
 
 x1,x2,x3,x4,y1,y2,y3,y4= 5,150,150,5,5,5,150,150
 xp1,xp2,xp3,xp4,yp1,yp2,yp3,yp4 = 100,200,220,100,100,80,80,200
@@ -63,10 +76,10 @@ A = np.array([[-x1,-y1,-1,0,0,0,x1*xp1,y1*xp1,xp1],
               [-x4,-y4,-1,0,0,0,x4*xp4,y4*xp4,xp4],
               [0,0,0,-x4,-y4,-1,x4*yp4,y4*yp4,yp4]])
 
-# VT, U, sigma, H = computeSVD(A)
-VT, U, H = computeSVD(A)
+VT, U, sigma, H = computeSVD(A)
+
 
 print("U matrix is: ", U)
 print("V transpose (VT) matrix is: ", VT)
-# print("Sigma (E) matrix is: ", sigma)
+print("Sigma (E) matrix is: ", sigma)
 print("Homography (SVD) matrix is: ", H)
