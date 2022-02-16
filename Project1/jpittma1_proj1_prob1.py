cd93c604b56cde1bbb58e9bf6fa08d27f03b07a5
Jerry Pittman Jr <jpittma1@umd.edu>
2022-02-16 07:43:54 -0500

    creation

---
+++ Project1/jpittma1_proj1_prob1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#ENPM673 Spring 2022
+#Section 0101
+#Jerry Pittman, Jr. UID: 117707120
+#jpittma1@umd.edu
+#Project #1
+
+
+#Problem#1- AR Detection and Decode
+
+#1a: AR Code Detection
+
+#1b: AR Code Decode
---
+++ Project1/jpittma1_proj1_prob2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#ENPM673 Spring 2022
+#Section 0101
+#Jerry Pittman, Jr. UID: 117707120
+#jpittma1@umd.edu
+#Project #1
+
+
+#Problem#2- Tracking
+
+#2a: Superimpose Image onto tag
+
+#2b: Placing virtual cube onto tag
